8f4038c271a625e1c5cff97f74b68e97f418a652
cris-andrade-97 <113953534+cris-andrade-97@users.noreply.github.com>
2022-09-19 22:12:55 -0300

    Delete mysqldb.py

--- database/mysqldb.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as conector
-
-conexion = conector.connect(
-    host="localhost",
-    user="root",
-    password="mysql",
-    database="instrumentosdb",
-    auth_plugin='mysql_native_password'
-)
